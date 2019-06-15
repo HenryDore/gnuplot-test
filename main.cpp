88682c3ad7da2bc8abb4baf641cc4fc12bb84bc2
HenryDore <henrymonkey@gmail.com>
2019-06-15 15:57:06 +0100

    changed to sine wave generator

--- main.cpp
+++ main.cpp
@@ -1,15 +1,24 @@
 #include <vector>
 #include "graph.h"
+#include <unistd.h>
+#include <math.h>
 using namespace std;
 
 int main(int argc,char **argv) {
+	vector<float> x,y;
 	plot p;
-	for(int a=0;a<100;a++) {
-		vector<float> x,y;
-		for(int k=a;k<a+200;k++) {
+
+	for(float a=0 ; a < 1000 ; a++) {
+		x.push_back(a);
+		y.push_back(100*sin(a/10));
+
+/*
+		for(int k=a ; k<a+200 ; k++) {
 			x.push_back(k);
-			y.push_back(k*k);
+			y.push_back(100*sin(k));
+			usleep(100);
 		}
+*/
 		p.plot_data(x,y);
 	}
 	return 0;
