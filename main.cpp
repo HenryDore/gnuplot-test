7420e3b3db5e18e5bb0ab982b89518b8aa598e7b
HenryDore <henrymonkey@gmail.com>
2019-06-15 20:41:33 +0100

    Made batch file for windows & formatting

--- main.cpp
+++ main.cpp
@@ -4,67 +4,57 @@
 #include <math.h>
 using namespace std;
 
+#ifdef _WIN32 // fine whatever windows you dickhead
+#include <windows.h>
 
-#ifdef _WIN32			//fine whatever windows you dickhead
-    #include <windows.h>
-
-    void sleep(unsigned milliseconds)
-    {
-        Sleep(milliseconds);
-    }
+void sleep(unsigned milliseconds) { Sleep(milliseconds); }
 #else
-    #include <unistd.h>
+#include <unistd.h>
 
-    void sleep(unsigned milliseconds)
-    {
-        usleep(milliseconds * 1000); // takes microseconds
-    }
+void sleep(unsigned milliseconds) {
+  usleep(milliseconds * 1000); // takes microseconds
+}
 #endif
 
-int main(int argc,char **argv) {
-	vector<float> x,y,xWindow,yWindow;
-	plot p;
-	x.reserve(100);
-	y.reserve(100);
-	xWindow.reserve(500);
-	yWindow.reserve(500);
+int main(int argc, char **argv) {
+  vector<float> x, y, xWindow, yWindow;
+  plot p;
+  x.reserve(100);
+  y.reserve(100);
+  xWindow.reserve(500);
+  yWindow.reserve(500);
 
-	bool initialised = 0;
-	for(int a=0 ; a < 100 ; a++) {
-		x.clear();
-		y.clear();
+  bool initialised = 0;
+  for (int a = 0; a < 100; a++) {
+    x.clear();
+    y.clear();
 
-		for(float j = 0 ; j < 100 ; j++) {
-			x.push_back(a+j/100);
-			y.push_back((100*sin(a+j/100))+100);
-		}
+    for (float j = 0; j < 100; j++) {
+      x.push_back(a + j / 100);
+      y.push_back((100 * sin(a + j / 100)) + 100);
+    }
 
+    if (a == 5)
+      initialised = 1;
 
-	if (a==5) initialised = 1;
+    xWindow.insert(xWindow.end(), x.begin(), x.end());
+    yWindow.insert(yWindow.end(), y.begin(), y.end());
 
-		xWindow.insert(xWindow.end(), x.begin(), x.end());
-		yWindow.insert(yWindow.end(), y.begin(), y.end());
+    if (initialised) {
+      xWindow.erase(xWindow.begin(), xWindow.begin() + 100);
+      yWindow.erase(yWindow.begin(), yWindow.begin() + 100);
+      xWindow.resize(500);
+      yWindow.resize(500);
+    }
 
-	if (initialised) {
-		xWindow.erase(xWindow.begin(),xWindow.begin()+100);
-		yWindow.erase(yWindow.begin(),yWindow.begin()+100);
-		xWindow.resize(500);
-		yWindow.resize(500);
-		}
+    p.plot_data(xWindow, yWindow);
+    sleep(1);
+  }
 
-
-	p.plot_data(xWindow,yWindow);
-
-	sleep(1);
-
-	}
-
-/*
-	for (unsigned int i = 0 ; i < 500 ; i++) {
-		cout << xWindow[i] << ", " << yWindow[i] << endl;
-	}
-*/
-
-	return 0;
-
+  /*
+          for (unsigned int i = 0 ; i < 500 ; i++) {
+                  cout << xWindow[i] << ", " << yWindow[i] << endl;
+          }
+  */
+  return 0;
 }
---
+++ run.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+ECHO Compiling like a motherfucking badass...
+g++ main.cpp -o main
+ECHO Running...
+main.exe
+ECHO All done! Have a FUCKING SMASHING day!
